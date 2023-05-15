9868d3f09bc0cb8091c6d11a327a27ec41e47c47
Tuan Nam <nttnnam@gmail.com>
2023-05-15 14:03:08 +0700

    refactor(loading screen): loading screen for appr

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Outlet } from "react-router-dom";
 
 // import component
 import Header from "./components/Header/header";
+import LoadingScreen from "./components/LoadingScreen/loadingScreen";
 
 const App = () => {
+  const [loadingScreen, setLoadingScreen] = useState(undefined);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoadingScreen(true);
+    }, 2000);
+  }, []);
+
   return (
-    <div className="app-container">
-      <div className="header-container">
-        <Header />
-      </div>
-      <div className="app-content">
-        <Outlet />
-      </div>
-    </div>
+    <>
+      {!loadingScreen ? (
+        <LoadingScreen
+          className="loading-screen"
+          type="bars"
+          color="black"
+          height={150}
+          width={375}
+        />
+      ) : (
+        <div className="app-container">
+          <div className="header-container">
+            <Header />
+          </div>
+          <div className="app-content">
+            <Outlet />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
