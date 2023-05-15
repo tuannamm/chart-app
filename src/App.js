42266b7c23f9611f6b66e7439b1d4b85d83e0ded
Tuan Nam <nttnnam@gmail.com>
2023-05-15 14:33:50 +0700

    feature(header): ui header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
+import "./App.scss";
 import { Outlet } from "react-router-dom";
 
 // import component
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,28 +1,46 @@
+import "./header.scss";
+
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { NavLink } from "react-bootstrap";
 
 const Header = () => {
+  const handleLogin = () => {};
+
+  const handleSignUp = () => {};
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="lg" className="header-container">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <NavLink to="/" className="nav-link navbar-brand ">
+          Chart
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavLink to="/user" className="nav-link">
+              User
+            </NavLink>
+            <NavLink to="/admin" className="nav-link">
+              Admin
+            </NavLink>
+          </Nav>
+
+          <Nav>
+            <button className="btn-login" onClick={() => handleLogin()}>
+              Log in
+            </button>
+            <button className="btn-signup" onClick={() => handleSignUp()}>
+              Sign up
+            </button>
+
+            <NavDropdown title="Setting" id="basic-nav-dropdown">
+              <NavDropdown.Item>Action</NavDropdown.Item>
+              <NavDropdown.Item>Another action</NavDropdown.Item>
+              <NavDropdown.Item>Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+              <NavDropdown.Item>Separated link</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
