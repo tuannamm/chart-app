eadb31dc681a4b28ad66167c114a332fe390782d
Tuan Nam <nttnnam@gmail.com>
2023-05-15 13:56:36 +0700

    feature(header): ui header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React from "react";
 import "./App.css";
+import { Outlet } from "react-router-dom";
 
-function App() {
-  return <div className="App"></div>;
-}
+// import component
+import Header from "./components/Header/header";
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <div className="header-container">
+        <Header />
+      </div>
+      <div className="app-content">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/layout.js
+++ src/layout.js
@@ -5,13 +5,16 @@ import "react-toastify/dist/ReactToastify.css";
 
 // import component
 import HomePage from "./components/HomePage/homepage";
+import App from "./App";
 
 const Layout = () => {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<App />}>
+            <Route index element={<HomePage />} />
+          </Route>
         </Routes>
       </Router>
     </>
